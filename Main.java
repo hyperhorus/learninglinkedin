885fa1a4dd479301f7912dba478d966526178bd3
hyperhorus <62409221+hyperhorus@users.noreply.github.com>
2020-07-13 19:26:29 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int studentAge= 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
-        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Kayla";
+        String studentLasttName = "Hammon";
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName + " " + studentLasttName + " " +
+                " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update to?");
     }
 }
