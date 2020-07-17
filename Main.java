511e7d39bb4fdc2cde43c96b3f6359acf42b5911
hyperhorus <62409221+hyperhorus@users.noreply.github.com>
2020-07-17 10:47:12 -0500

    Add files via upload
    Funciones en java

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        int studentAge= 15;
-        double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
-        boolean hasPerfectAttendance = true;
+import java.util.Scanner;
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
+public class Main {
+
+    public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate){
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        System.out.println("Your total meal price is: " + result);
+        return  result;
+    }
+    public static void main(String[] args) {
+       // calculateTotalMealPrice(25.90,.10,.10);
+       // calculateTotalMealPrice(25,.18,.08);
+        double totalPrice  = calculateTotalMealPrice(100,.2,.08);
+        System.out.println("Total per person: " + totalPrice/5 );
+
     }
 }
